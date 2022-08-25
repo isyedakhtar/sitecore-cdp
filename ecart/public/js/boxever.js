babd9a1bfeee2c6d97ab1652214e2fbe55e41723
abi82 <110359301+abi82@users.noreply.github.com>
2022-08-25 15:31:03 +1000

    Update boxever.js

--- ecart/public/js/boxever.js
+++ ecart/public/js/boxever.js
@@ -1,3 +1,20 @@
+(function() {
+    // Define the Sitecore CDP (Boxever) settings
+    window._boxeverq = window._boxeverq || [];
+    window._boxever_settings = {
+        client_key: UAT_CLIENT_KEY,
+        target: API_TARGET,
+        cookie_domain: COOKIE_DOMAIN,
+        web_flow_target: WEB_FLOW_TARGET,
+        pointOfSale: POINT_OF_SALE
+    };
 
-
-
+    // Import the Boxever library asynchronously
+    (function () {
+        var s = document.createElement("script");
+        s.type = "text/javascript";
+        s.async = false;
+        s.src = SCRIPT_SOURCE;
+        var x = document.getElementsByTagName("script")[0];
+        x.parentNode.insertBefore(s, x);
+    })();
